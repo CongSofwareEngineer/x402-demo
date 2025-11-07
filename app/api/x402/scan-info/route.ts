59173cea005b11fa2c71081fc3e655d101d37c73
congbacoor <cong@bacoor.co>
2025-11-07 11:03:43 +0700

    fix scan data

---
+++ app/api/x402/scan-info/route.ts
@@ -0,0 +1,23 @@
+// export { POST } from 'x402-next'
+
+import { NextRequest } from 'next/server'
+
+import X402ScanService from '@/services/x402scan'
+
+export async function GET(req: NextRequest) {
+  try {
+    const resFirstTx = await X402ScanService.getFirstTransferTimestamp()
+
+    const res = await X402ScanService.getInfo(resFirstTx)
+
+    return new Response(
+      JSON.stringify({
+        data: res,
+      })
+    )
+  } catch (error) {
+    console.error('Error in /api/x402/demo:', error)
+
+    return new Response(JSON.stringify({ error: 'Internal Server Error' }), { status: 500 })
+  }
+}
